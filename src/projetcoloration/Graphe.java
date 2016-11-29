76b618f7921c66cbd1dc3a4524c6c800cfb2c617
Iguane42 <bocquet.yoann@gmail.com>
2016-11-29 15:24:54 +0100

    Création de la structure de graphe

--- src/projetcoloration/Graphe.java
+++ src/projetcoloration/Graphe.java
@@ -5,10 +5,35 @@
  */
 package projetcoloration;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Epulapp
  */
 public class Graphe {
     
+    private ArrayList<Sommet> sommets;
+    
+    public Graphe() {
+        this.sommets = new ArrayList<>();
+    }
+    
+    public Graphe(ArrayList<Sommet> sommets) {
+        this.sommets = sommets;
+    }
+
+    /**
+     * @return the sommets
+     */
+    public ArrayList<Sommet> getSommets() {
+        return sommets;
+    }
+    
+    public void relierSommets(int indice1, int indice2) {
+        Sommet sommet1 = sommets.get(indice1);
+        Sommet sommet2 = sommets.get(indice2);
+        sommet1.getVoisins().add(sommet2);
+        sommet2.getVoisins().add(sommet1);
+    }
 }
